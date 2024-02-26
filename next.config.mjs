a0f412b8824abe1843e73a585edb0b743f0daee8
JasonWijaya2 <jasonwijaya2@gmail.com>
2024-02-26 20:21:49 +0700

    experimental

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    forceSwcTransforms: true,
+  },
   images: {
     domains: ['media.steelseriescdn.com', 'assets3.razerzone.com', 'resource.logitechg.com', 'resource.logitech.com', 'hkgaming.hermanmiller.com', 'asset-us-store.msi.com', 'lh3.googleusercontent.com'],
   },
